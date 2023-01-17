0dbb5aa5e199aeb2b5abf64b23f7b546d844563d
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2023-01-17 17:27:38 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+
+import FeedbackOptions from "./CaffeApp/FeedbackOptions";
+import Statistics from "./CaffeApp/Statistics";
+
+class App  extends Component {
+  state = {
+    good: 3,
+    neutral: 4,
+    bad: 2       
+  }
+
+  countTotalFeedback = () => this.state.good + this.state.neutral + this.state.bad;
+
+  countPositiveFeedbackPercentage = () => Math.round((this.state.good / this.countTotalFeedback()) * 100);
+
+  render() {
+    return (
+      <div>
+        <h2>Pleace leave feedback</h2>
+
+        <div>
+          <FeedbackOptions options={"#"} onLeaveFeedback={"#"} />
+
+          {/* <button type="button">Good</button>
+          <button type="button">Neutral</button>
+          <button type="button">Bad</button> */}
+        </div>
+
+        <div>
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+        </div>
+      </div>
+    );
+  }
 };
+export default App ;
---
+++ src/components/CaffeApp/FeedbackOptions.jsx
@@ -0,0 +1,14 @@
+
+
+const FeedbackOptions = (props) => {
+    return (
+        <>
+            <button type="button">Good</button>
+            <button type="button">Neutral</button>
+            <button type="button">Bad</button>
+        </>
+    );
+};
+export default FeedbackOptions;
+
+// 
